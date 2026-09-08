e09ad67702e7f92874049136f0d47a5605a2f1ce
Purrity Gwabeni <purritygwabeni43@gmail.com>
2026-09-08 10:12:34 +0200

    signup auth

---
+++ Rentosphere-Backend/routes/auth.js
@@ -0,0 +1,13 @@
+import express from 'express'
+import {
+  signup
+} from '../controllers/authController.js'
+
+const router = express.Router()
+
+
+// POST /api/auth/signup
+router.post('/signup', signup)
+
+
+export default router
